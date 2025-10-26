725fa28473f6963db41aedf8236404c0ecc33f3d
ng-caixian <ngcaixian@gmail.com>
2025-10-26 23:01:37 +0800

    remove enumerations folder

--- enumerations/ApplicationStatus.java
+++
@@ -1,8 +0,0 @@
-package enumerations;
-
-public enum ApplicationStatus {
-	PENDING,
-	SUCCESSFUL,
-	UNSUCCESSFUL,
-	WITHDRAWN
-}
--- enumerations/ApprovalStatus.java
+++
@@ -1,7 +0,0 @@
-package enumerations;
-
-public enum ApprovalStatus {
-	PENDING,
-	APPROVED,
-	REJECTED
-}
